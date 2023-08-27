bd70726d60dc9186ded3d52953886ae459eb56b3
Aidys Pushkunov <mywayinprogramming@gmail.com>
2023-08-27 21:52:43 +0700

    added packeges to package.json lexical/code

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -6,18 +6,20 @@ import { FunctionComponent } from "react";
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
-import { CodeHighlightNode, CodeNode } from "./node";
+// import { CodeHighlightNode } from "@lexical/code";
+import { CodeNode, CodeHighlightNode } from './node';
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { HeadingNode } from "@lexical/rich-text";
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { CodeHighlightPlugin } from "./plugins/CodeHighlightPlugin";
 import { PlaygroundEditorTheme } from "./themes/playgroundEditorThemes";
 import { LimeEditorTheme } from "./themes/limeEditorTheme";
-import { DraculaEditorTheme } from "./themes/dracula"
 import { ClipBoard } from "@/components/clipboard";
 import { StoryCircle } from "@/components/storyСircle";
-import { $getRoot } from 'lexical';
+
+import { $getRoot } from "lexical";
 import { $createCodeNode } from "./node";
+
 
 const onChange = (editorState: any) => {
     editorState.read(() => {
@@ -26,7 +28,7 @@ const onChange = (editorState: any) => {
     })
 }
 
-const initialCode = () => {
+function initialCode() {
     const root = $getRoot();
     if (root.getFirstChild() === null) {
         const codeNode = $createCodeNode();
@@ -34,10 +36,11 @@ const initialCode = () => {
     }
 }
 
+
 const Editor: FunctionComponent = () => {
 
-    const EMPTY_CONTENT =
-        '{"root":{"children":[{"children":[],"direction":null,"format":"code","indent":0,"type":"code","version":1}],"direction":null,"format":"code","indent":0,"type":"root","version":1}}';
+    // const EMPTY_CONTENT =
+    //     '{"root":{"children":[{"children":[],"direction":null,"format":"code","indent":0,"type":"code","version":1}],"direction":null,"format":"code","indent":0,"type":"root","version":1}}';
 
 
     const initialConfig = {
@@ -54,6 +57,8 @@ const Editor: FunctionComponent = () => {
             <div className="relative w-[85vw] sm:w-[60vw] h-[80%] text-[20px] drop-shadow-xl">
                 <div className="flex justify-between items-center">
                     <div className="flex mb-10">
+                        <StoryCircle />
+                        <StoryCircle />
                         <StoryCircle />
                         <StoryCircle />
                         <StoryCircle />
@@ -90,3 +95,4 @@ const Editor: FunctionComponent = () => {
 };
 
 export { Editor };
+
